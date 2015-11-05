90222cd93820fe7b9b7d2875c14e66a214db9d94
JustinHLei <lei0401@gmail.com>
2015-11-05 10:17:31 -0600

    Created the Project and Downloaded a new bootstrap theme

---
+++ Justin H. Lei's Blog/Justin H. Lei's Blog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Justin_H.Lei_s_Blog.MvcApplication" Language="C#" %>
---
+++ Justin H. Lei's Blog/Justin H. Lei's Blog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Justin_H.Lei_s_Blog.Startup))]
+namespace Justin_H.Lei_s_Blog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
